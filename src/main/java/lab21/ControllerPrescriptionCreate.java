193d9ffd11f45c02bbcbed02d49a01d3cec594d4
Gagan Rampur <grampur123@gmail.com>
2023-12-10 19:04:54 -0800

    3rd commit

--- src/main/java/lab21/ControllerPrescriptionCreate.java
+++ src/main/java/lab21/ControllerPrescriptionCreate.java
@@ -50,40 +50,58 @@ public class ControllerPrescriptionCreate {
 	public String createPrescription(PrescriptionView p, Model model) {
 
 	    // Validate doctor name and id
-	    Doctor doctor = doctorRepository.findByIdAndFirstNameAndLastName(p.getDoctor_id(),p.getDoctorFirstName(), p.getDoctorLastName());
+	    Doctor doctor = doctorRepository.findByIdAndFirstNameAndLastName(p.getDoctor_id(), p.getDoctorFirstName(), p.getDoctorLastName());
 	    if (doctor == null) {
 	        model.addAttribute("message", "Invalid doctor name or ID.");
-	        return "prescription_create";
+	        return "index";
 	    }
 
 	    // Validate patient name and id
 	    Patient patient = patientRepository.findByIdAndFirstNameAndLastName(p.getPatient_id(), p.getPatientFirstName(), p.getPatientLastName());
 	    if (patient == null) {
 	        model.addAttribute("message", "Invalid patient name or ID.");
-	        return "prescription_create";
+	        return "index";
 	    }
 
 	    // Validate drug name
 	    Drug drug = drugRepository.findByName(p.getDrugName());
 	    if (drug == null) {
 	        model.addAttribute("message", "Invalid drug name.");
-	        return "prescription_create";
+	        return "index";
 	    }
+	    
+	    
 
 	    // Insert prescription
 	    Prescription prescription = new Prescription();
 	    prescription.setRxid(sequence.getNextSequence("RXID_SEQUENCE"));
-	    prescription.setDrugName(drug.getName());
+	    prescription.setDrugName(p.getDrugName());
 	    prescription.setQuantity(p.getQuantity());
-	    prescription.setPatient_id(patient.getId());
-	    prescription.setDoctor_id(doctor.getId());
+	    prescription.setPatient_id(p.getPatient_id());
+	    prescription.setDoctor_id(p.getDoctor_id());
 	    prescription.setDateCreated(LocalDate.now().toString());
 	    prescription.setRefills(p.getRefills());
 	    
 	    prescriptionRepository.save(prescription);
 
+	    // Create a new PrescriptionView object to send back to the view
+	    PrescriptionView prescriptionView = new PrescriptionView();
+	    prescriptionView.setRxid(prescription.getRxid());
+	    prescriptionView.setDrugName(prescription.getDrugName());
+	    prescriptionView.setQuantity(prescription.getQuantity());
+	    prescriptionView.setPatient_id(prescription.getPatient_id());
+	    prescriptionView.setDoctor_id(prescription.getDoctor_id());
+	    prescriptionView.setDateCreated(prescription.getDateCreated());
+	    prescriptionView.setRefills(prescription.getRefills());
+
+	    // Assuming you have setters for doctor and patient names in PrescriptionView
+	    prescriptionView.setDoctorFirstName(doctor.getFirstName());
+	    prescriptionView.setDoctorLastName(doctor.getLastName());
+	    prescriptionView.setPatientFirstName(patient.getFirstName());
+	    prescriptionView.setPatientLastName(patient.getLastName());
+
 	    model.addAttribute("message", "Prescription created.");
-	    model.addAttribute("prescription", prescription);
+	    model.addAttribute("prescription", prescriptionView); // Pass the PrescriptionView object
 	    return "prescription_show";
 	}
 
